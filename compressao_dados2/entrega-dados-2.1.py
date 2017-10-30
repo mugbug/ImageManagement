0cd06ad2db9d24e1d1b23792bb892a02cb88b711
Pedro Zaroni <zaronipedro@outlook.com>
2017-10-29 23:52:33 -0200

    Add RLE decoder

---
+++ compressao_dados2/entrega-dados-2.1.py
@@ -0,0 +1,44 @@
+RLE = []
+
+print('Entre com uma matriz para comprimir ou pressione enter para usar uma matriz default:')
+
+nRow = input('Numero de linhas da matriz:')
+if nRow != '':
+    nRow = int(nRow)
+    for i in range(nRow):
+        row = [int(x) for x in input().split()]
+        RLE.append(row)
+
+if RLE == [] or nRow == '':
+    RLE = [
+        [0, 1, 1, 2],
+        [0, 4],
+        [1, 1, 2],
+        [1, 2, 1],
+        [4]
+    ]
+
+r = len(RLE)
+
+matrix = [[] for i in range (r)]
+
+for i in range(r):
+    c = len(RLE[i])
+    if RLE[i][0] == 0:
+        previous = 0
+        k=1
+    else:
+        previous = 1
+        k=0
+    for j in range(k,c):
+        current = RLE[i][j]
+        while current != 0:
+            matrix[i].append(previous)
+            current -= 1
+        
+        if previous == 1:
+            previous = 0
+        else:
+            previous = 1
+
+print('Matriz comprimida:   {}\nMatriz original: {}'.format(RLE, matrix))
